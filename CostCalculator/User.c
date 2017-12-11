13542264d92df9d9b59ea2581a1d849465cef40b
Nemanja Vrljanovic <nemanjavrljanovic96@gmail.com>
2017-12-11 19:10:30 +0100

    Add Remove User, new (help) functions

--- CostCalculator/User.c
+++ CostCalculator/User.c
@@ -71,3 +71,155 @@ void addNewUser()
 		printf("Error while opening file.");
 	}
 }
+
+char* inputPin()
+{
+	char c, *pin = (char *)calloc(15, sizeof(char));
+	int pos = 0;
+	do {
+		c = _getch();
+
+		if (isprint(c) && c >= 0x30 && c <= 0x39)
+		{
+			pin[pos++] = c;
+			printf("%c", '*');
+		}
+		else if (c == 8 && pos)
+		{
+			pin[pos--] = '\0';
+			printf("%s", "\b \b");
+		}
+	} while (c != 13);
+	return pin;
+}
+
+int removeUser()
+{
+	int numberOfRegisteredUsers = 0;
+	User *users = readAllRegisteredUsers(&numberOfRegisteredUsers), toDeleteUser;
+	printAllUsers();
+	printf("Deleting user account\n");
+	printf("Name: ");
+	scanf("%s", toDeleteUser.name);
+	printf("Surname: ");
+	scanf("%s", toDeleteUser.surname);
+	printf("Username: ");
+	scanf("%s", toDeleteUser.username);
+	int i = isUsernameExisting(toDeleteUser.username);
+	if (i >= 0 && !strcmp(users[i].name, toDeleteUser.name) && !strcmp(users[i].surname, toDeleteUser.surname))
+	{
+		char answer;
+		getchar();
+		printf("Do you really want do delete user[Y/N]");
+		scanf("%c", &answer);
+		if (answer == 'Y' || answer == 'y')
+		{
+			for (int j = i; j < numberOfRegisteredUsers - 1; ++j)
+				users[j] = users[j + 1];
+			printAllUsersInFile(users, numberOfRegisteredUsers - 1);
+			return 1;
+		}
+		return 0;
+	}
+	printf("No such user.");
+	return 0;
+}
+
+User * readAllRegisteredUsers(int *numberOfAllUsers)
+{
+	int numberOfRegisteredUsers = 0, temp = 1;
+	User *allUsers = (User *)calloc(temp, sizeof(User));
+	FILE *users = fopen("users.txt", "r");
+	char group[8] = { 0 };
+	if (users)
+	{
+		while (fscanf(users, "%s %s %s %x %s\n", allUsers[numberOfRegisteredUsers].name, allUsers[numberOfRegisteredUsers].surname, allUsers[numberOfRegisteredUsers].username, &allUsers[numberOfRegisteredUsers].pin, group) != EOF)
+		{
+			if (!strcmp(group, "Admin"))
+			{
+				allUsers[numberOfRegisteredUsers].userGroup = Admin;
+			}
+			else
+			{
+				allUsers[numberOfRegisteredUsers].userGroup = Analyst;
+			}
+			++numberOfRegisteredUsers;
+			if (numberOfRegisteredUsers == temp)
+			{
+				allUsers = (User *)realloc(allUsers, sizeof(User) * (temp *= 2));
+			}
+
+		}
+		if (numberOfRegisteredUsers == 0)
+		{
+			*numberOfAllUsers = 0;
+			return NULL;
+		}
+		allUsers = (User *)realloc(allUsers, sizeof(User) * (1 + numberOfRegisteredUsers));
+		(*numberOfAllUsers) = numberOfRegisteredUsers;
+		fclose(users);
+		return allUsers;
+	}
+	else printf("Error while opening users.txt");
+	return NULL;
+}
+
+void printAllUsersInFile(User *users, int numberOfUsers)
+{
+	FILE *file = fopen("users.txt", "w");
+	if (!file)
+	{
+		printf("Error while opening users.txt");
+		return;
+	}
+	for (int i = 0; i < numberOfUsers; ++i)
+	{
+		if (users[i].userGroup == Admin)
+		{
+			fprintf(file, "%-25s %-25s %-25s %-15x %-5s\n", users[i].name, users[i].surname, users[i].username, users[i].pin, "Admin");
+		}
+		else
+		{
+			fprintf(file, "%-25s %-25s %-25s %-15x %-7s\n", users[i].name, users[i].surname, users[i].username, users[i].pin, "Analyst");
+		}
+	}
+	fclose(file);
+}
+
+int isUsernameExisting(char *username)
+{
+	int numberOfAllUsers;
+	User *allUsers = readAllRegisteredUsers(&numberOfAllUsers);
+	for (int i = 0; i < numberOfAllUsers; ++i)
+	{
+		if (!strcmp(allUsers[i].username, username))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void printAllUsers()
+{
+	int numberOfUsers = 0;
+	User *allUsers = readAllRegisteredUsers(&numberOfUsers);
+	if (!allUsers)
+	{
+		return;
+	}
+	printf("All Users:\n");
+	printf("%-25s %-25s %-25s %-20s\n=========================================================================================\n", "Name", "Surname", "Username", "User Group");
+	for (int i = 0; i < numberOfUsers; ++i)
+	{
+		if (allUsers[i].userGroup == Admin)
+		{
+			printf("%-25s %-25s %-25s %-7s\n", allUsers[i].name, allUsers[i].surname, allUsers[i].username, "Admin");
+		}
+		else
+		{
+			printf("%-25s %-25s %-25s %-7s\n", allUsers[i].name, allUsers[i].surname, allUsers[i].username, "Analyst");
+		}
+	}
+	printf("=========================================================================================\n");
+}
---
+++ CostCalculator/main.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "User.h"
+//Testing functions
+int main()
+{
+	//Add 3 Users, and remove one
+	addNewUser();
+	addNewUser();
+	addNewUser();
+	removeUser();
+	return 0;
+}
