e2a0fcb2f7ee7f12026ac6f1a33a8016653ae253
Nikola Moraca <34371226+lilFishy@users.noreply.github.com>
2018-01-16 18:29:37 +0100

    Assembly of the analyser parts, and merging with adminsitrative part

--- CostCalculator/main.c
+++ CostCalculator/main.c
@@ -5,15 +5,23 @@
 #include "Currency.h"
 #include "Bill.h"
 
-User *currentUser;
+
+void makelog(char *, User *, FILE *);
+//int main()
+//{
+//	Currency currentCurrency = { "CHF", 0.603};
+//	Node *head = NULL, *tail = NULL;
+//	loadBills(&head, &tail);
+//	exportDataForBuyer(head, tail, currentCurrency);
+//	exportDataForMonth(head, tail, currentCurrency);
+//	exportDataForProduct(head, tail, currentCurrency);
+//}
+
+Currency currentCurrency = { "BAM", 1.00 };
 
 int main()
 {
-	/*Node *head = NULL, *tail = NULL;
-	loadBills(&head, &tail);
-	for (Node *tmp = head; tmp; tmp = tmp->next)
-		printf("%s -- %d\n", tmp->bill.date, isDateLegitimate(tmp->bill.date));
-	return 0;*/
+	User *currentUser = NULL;
 	int option;
 	time_t rawtime;
 	struct tm *timeinfo;
@@ -36,103 +44,69 @@ int main()
 		if (!adminlog)
 		{
 			printf("Unexpected Error");
-			Sleep(3000);
 			exit(-1);
 		}
-		time(&rawtime);
-		timeinfo = localtime(&rawtime);
-		fprintf(adminlog, "%s Login %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-
+		makelog("Login", currentUser, adminlog);
 		while (1)
 		{
-			printf("\nChoose one of options:\n\n");
-			printf("[1] Add New User\n[2] Remove User\n[3] Change Users Group\n[4] Print All Users\n[5] Add New Currency\n[6] Remove Currency\n[7] Change Currency\n[8] Update Currency's rate\n[9] Print All Currencies\n[10] Logout\n[0] Close Application\n");
+			printf("Choose one of options:\n\n");
+			printf("[1] Add New User\n[2] Remosve User\n[3] Change Users Group\n[4] Print All Users\n[5] Add New Currency\n[6] Remove Currency\n[7] Change Currency\n[8] Update Currency's rate\n[9] Print All Currencies\n[10] Logout\n[0] Close Application\n");
 			scanf("%d", &option);
 			switch (option)
 			{
 			case 1:
 				addNewUser();
-				time(&rawtime);
-				timeinfo = localtime(&rawtime);
-				fprintf(adminlog, "%s Add New User %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+				makelog("Add New User", currentUser, adminlog);
 				system("cls");
 				break;
 			case 2:
-				removeUser(currentUser);
-				time(&rawtime);
-				timeinfo = localtime(&rawtime);
-				fprintf(adminlog, "%s Remove User %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+				removeUser();
+				makelog("Remove User", currentUser, adminlog);
 				system("cls");
 				break;
 			case 3:
-			{
-				User *changedUser = changeUserGroup();
-				if (!strcmp(currentUser->username, changedUser->username))
-				{
-					currentUser = NULL;
-					printf("\nLogin to continue\n");
-					Sleep(2000);
-					main();
-				}
-				time(&rawtime);
-				timeinfo = localtime(&rawtime);
-				fprintf(adminlog, "%s Change User Group %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+				changeUserGroup();
+				makelog("Change Users Group", currentUser, adminlog);
 				system("cls");
 				break;
-			}
 			case 4:
 				system("cls");
-				time(&rawtime);
-				timeinfo = localtime(&rawtime);
-				fprintf(adminlog, "%s Print All Users %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+				makelog("Print All Users", currentUser, adminlog);
 				printAllUsers();
 				break;
 			case 5:
 				addNewCurrency();
-				time(&rawtime);
-				timeinfo = localtime(&rawtime);
-				fprintf(adminlog, "%s Add New Currency %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+				makelog("Add New Currency", currentUser, adminlog);
 				system("cls");
 				break;
 			case 6:
 				removeCurrency();
-				time(&rawtime);
-				timeinfo = localtime(&rawtime);
-				fprintf(adminlog, "%s Remove Currency %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+				makelog("Remove Currency", currentUser, adminlog);
 				system("cls");
 				break;
 			case 7:
 				changeCurrency();
-				time(&rawtime);
-				timeinfo = localtime(&rawtime);
-				fprintf(adminlog, "%s Change Currency %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+				makelog("Change Currency", currentUser, adminlog);
 				system("cls");
 				printf("\n%s %lf\n", currentCurrency.currency, currentCurrency.currencyRate);
 				break;
 			case 8:
 				updateCurrencyRate(NULL);
-				time(&rawtime);
-				timeinfo = localtime(&rawtime);
-				fprintf(adminlog, "%s Update Currency Rate %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+				makelog("Update Currency Rate", currentUser, adminlog);
 				system("cls");
 				break;
 			case 9:
 				system("cls");
-				time(&rawtime);
-				timeinfo = localtime(&rawtime);
-				fprintf(adminlog, "%s Print All Currencies %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+				makelog("Print All Currencies", currentUser, adminlog);
 				printAllCurrencies();
 				break;
 			case 10:
 				system("cls");
-				time(&rawtime);
-				timeinfo = localtime(&rawtime);
-				fprintf(adminlog, "%s Logout %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+				makelog("Logout", currentUser, adminlog);
 				fclose(adminlog);
-				currentUser = NULL;
 				main();
 				break;
-			default:
+			default: case 0:
 				fclose(adminlog);
 				return 0;
 			}
@@ -145,18 +119,63 @@ int main()
 		system("cls");
 		printf("Welcome %s", currentUser->name);
 		printf("\nLogin time: %02d.%02d.%02d. - %02d:%02d:%02d\n", timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-		printf("Analyst part of application is under constrquction.");
 		FILE *analystlog = fopen("analystlog.txt", "a+");
 		if (!analystlog)
 		{
 			printf("Unexpected Error");
-			Sleep(2000);
 			exit(-1);
 		}
-		time(&rawtime);
-		timeinfo = localtime(&rawtime);
-		fprintf(analystlog, "%s Login %02d.%02d.%02d. - %02d:%02d:%02d\n", currentUser->username, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-		fclose(analystlog);
-		main();
+		makelog("Login", currentUser, analystlog);
+		Node *head = NULL, *tail = NULL;
+		loadBills(&head, &tail);
+		while (1)
+		{
+			printf("Choose one of options:\n\n");
+			printf("[1] Export Data For Product\n[2] Export Data For Month\n[3] Export Data for Buyer\n[4] Change Currency\n[5] Logout\n[0] Close Application\n");
+			scanf("%d", &option);
+			switch (option)
+			{
+			case 1:
+				exportDataForProduct(head, tail, currentCurrency);
+				makelog("Export Data For Product", currentUser, analystlog);
+				system("cls");
+				break;
+			case 2:
+				exportDataForMonth(head, tail, currentCurrency);
+				makelog("Export Data For Month", currentUser, analystlog);
+				system("cls");
+				break;
+			case 3:
+				exportDataForBuyer(head, tail, currentCurrency);
+				makelog("Export Data For Buyer", currentUser, analystlog);
+				system("cls");
+				break;
+			case 4:
+				changeCurrency();
+				makelog("Change Currency", currentUser, analystlog);
+				system("cls");
+				printf("\n%s %lf\n", currentCurrency.currency, currentCurrency.currencyRate);
+				break;
+			case 5:
+				system("cls");
+				makelog("Logout", currentUser, analystlog);
+				fclose(analystlog);
+				clearList(&head, &tail);
+				main();
+			default:case 0:
+				fclose(analystlog);
+				clearList(&head, &tail);
+				return 0;
+			}
+		}
 	}
+}
+
+void makelog(char *message, User *user, FILE *file)
+{
+	time_t rawtime;
+	struct tm *timeinfo;
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	fprintf(file, "%s %s %02d.%02d.%02d. - %02d:%02d:%02d\n", user->username, message, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 }
